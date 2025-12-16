1489b5ce9e5fe4cf2bce372f7e129864537e2c49
khb532 <khb532@naver.com>
2025-12-16 11:45:12 +0900

    [FEAT] Web Browser

---
+++ Source/UE_ReNe/Public/Widget/Rene_WebBrowser.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WebBrowser.h"
+#include "Rene_WebBrowser.generated.h"
+
+UCLASS()
+class UE_RENE_API URene_WebBrowser : public UWebBrowser
+{
+	GENERATED_BODY()
+	
+	/* Method */
+public:
+	
+	
+	
+	
+	
+private:
+	
+	
+	
+	
+	
+	/* Field */
+public:
+	
+	
+	
+	
+	
+private:
+	
+	
+	
+	
+	
+	
+};
