ea5f618228746bf43facbd89c10b024e74148456
kleo19 <kleo19@naver.com>
2026-01-05 10:38:59 +0900

    beta_end interview fix

--- Source/UE_ReNe/Private/Player/Rene_PlayerController.cpp
+++ Source/UE_ReNe/Private/Player/Rene_PlayerController.cpp
@@ -666,8 +666,16 @@ void ARene_PlayerController::EndInterview()
 
 void ARene_PlayerController::ServerRPC_RequestStandUp_Implementation()
 {
+	// Restore the camera view to the character
+	if (APawn* PlayerPawn = GetPawn())
+	{
+		SetViewTargetWithBlend(PlayerPawn, 0.8f);
+	}
+
 	if (AUE_ReNeCharacter* ControlledCharacter = Cast<AUE_ReNeCharacter>(GetPawn()))
+	{
 		ControlledCharacter->StandUp();
+	}
 }
 
 void ARene_PlayerController::OnAIMessageReceived(const FString& AIMessage)
