66c72bcf5d93a3752a875659c6daef4dce90a5db
Dove9 <khb532@naver.com>
2026-05-13 02:10:09 +0900

    -

--- Source/UE_ReNe/FE/FE_GameplayTags.cpp
+++ Source/UE_ReNe/FE/FE_GameplayTags.cpp
@@ -2,7 +2,7 @@
 
 namespace FE_GameplayTags
 {
-	// UE_DEFINE_GAMEPLAY_TAG(header:Name, "Editor View Name : Category.SubCat.SubSubCat");
+	// UE_DEFINE_GAMEPLAY_TAG(header : Var, "Editor View Name : Category.SubCat.SubSubCat");
 	UE_DEFINE_GAMEPLAY_TAG(FE_WidgetStack_Modal, "FE.WidgetStack.Modal");
 	UE_DEFINE_GAMEPLAY_TAG(FE_WidgetStack_GameMenu, "FE.WidgetStack.GameMenu");
 	UE_DEFINE_GAMEPLAY_TAG(FE_WidgetStack_HUD, "FE.WidgetStack.HUD");
